68042c0877a69aadb42a864610caa3d21729b2e3
Sadeepa11 <srisadeepa658@gmail.com>
2024-09-03 11:31:30 +0530

    Initial commit

---
+++ src/model/Subject.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author stf
+ */
+public class Subject {
+    
+    private String id;
+    private String name;
+    private String fee;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFee() {
+        return fee;
+    }
+
+    public void setFee(String fee) {
+        this.fee = fee;
+    }
+    
+    
+}
